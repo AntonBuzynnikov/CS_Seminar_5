c803528d36a9730b806ca2f73300c3963c17ea18
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-12 19:48:51 +0700

    Complete Example050

---
+++ Example050/Program.cs
@@ -0,0 +1,9 @@
+﻿// 50. Выяснить являются ли 3 числа сторонами треугольника
+int a,b,c;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+if(a+b > c && a+c > b && c+b > a)
+    System.Console.WriteLine("Являются");
+else
+    System.Console.WriteLine("Не являются");
